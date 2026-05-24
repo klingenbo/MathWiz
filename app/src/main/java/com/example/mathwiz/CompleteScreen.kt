7e525faaaea1d8c90ea5c5c8321db1d401937f69
klingenbo <lina.klingberg@gmail.com>
2026-05-24 23:09:24 +0200

    prep: add outlined buttons
    Outlined buttons was added for the options Play again and Go to start to make the app more intuitive

--- app/src/main/java/com/example/mathwiz/CompleteScreen.kt
+++ app/src/main/java/com/example/mathwiz/CompleteScreen.kt
@@ -1,26 +1,23 @@
 package com.example.mathwiz
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -48,7 +45,7 @@ fun CompleteScreen(
 
         )
 
-        Spacer(modifier = Modifier.height(50.dp))
+        Spacer(modifier = Modifier.height(40.dp))
 
         Box(
             modifier = Modifier
@@ -81,16 +78,12 @@ fun CompleteScreen(
 
         Spacer(modifier = Modifier.height(40.dp))
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .background(Color(0xFF90CAF9))
-                .shadow(4.dp, RoundedCornerShape(16.dp))
-                .clip(RoundedCornerShape(16.dp))
-                .clickable { playAgain() }
-                .padding(24.dp),
-            contentAlignment = Alignment.Center
+        OutlinedButton(
+            onClick = { playAgain() },
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = Color(0xFF6650A4),
+                contentColor = Color.White
+            )
         ) {
             Text(
                 text = "Gör igen",
@@ -98,16 +91,14 @@ fun CompleteScreen(
             )
         }
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .background(Color(0xFFA5D6A7))
-                .shadow(4.dp, RoundedCornerShape(16.dp))
-                .clip(RoundedCornerShape(16.dp))
-                .clickable { openStart() }
-                .padding(24.dp),
-            contentAlignment = Alignment.Center
+        Spacer(modifier = Modifier.height(40.dp))
+
+        OutlinedButton(
+            onClick = { openStart() },
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = Color(0xFF6650A4),
+                contentColor = Color.White
+            )
         ) {
             Text(
                 text = "Till start",
